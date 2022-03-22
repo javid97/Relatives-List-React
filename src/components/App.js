2c2cf480e483f59c690feeee50be11107393b7eb
Javid Raina <rainajavid15@gmail.com>
2022-03-22 15:19:57 +0530

    Added key to orderedList

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,22 @@
-import React, {Component, useState} from "react";
+import React, {Component} from "react";
 import '../styles/App.css';
 import List from "./List";
 
 class App extends Component {
     render() {
         const relativeList = ['Relative 1', 'Relative 2', 'Relative 3', 'Relative 4', 'Relative 5', 'Relative 6'];
-        return(
-            <div id="main">
-                <ol>
-                    {
-                        relativeList.map((relative, idx) => <List key={`relativeListItem${idx+1}`} relative={relative} />)
-                    }
-                </ol>
-            </div>
-        )
+        return (
+          <div id="main">
+            <ol key="relativeList">
+              {relativeList.map((relative, idx) => (
+                <List
+                  key={`relativeListItem${idx+1}`}
+                  relative={relative}
+                />
+              ))}
+            </ol>
+          </div>
+        );
     }
 }
 
