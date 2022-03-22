7d7d04ffe0b2a75908b1a9a31bbbcc3e5b786c63
Javid Raina <rainajavid15@gmail.com>
2022-03-22 15:12:51 +0530

    Updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,17 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
+import List from "./List";
 
 class App extends Component {
     render() {
-
+        const relativeList = ['Relative 1', 'Relative 2', 'Relative 3', 'Relative 4', 'Relative 5', 'Relative 6'];
         return(
             <div id="main">
-               {/* Do not remove the main div */}
+                <ol>
+                    {
+                        relativeList.map((relative, idx) => <List key={`relativeListItem${idx+1}`} relative={relative} />)
+                    }
+                </ol>
             </div>
         )
     }
---
+++ src/components/List.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function List(props) {
+  return (
+    <li>{props.relative}</li>
+  )
+}
+
+export default List
